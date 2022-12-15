630a79b7d2ffddf96772b2c41c6d0d866083e2ba
VENKATESWARA RAO DUNNE <venkatesh.dunne@gmail.com>
2022-12-15 21:51:14 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
+const forecast=require('../utils/forecast')
+const geocode=require('../utils/geocode')
+
+const app=express()
+const port=process.env.PORT || 3000
+
+//Define paths for express config
+const publicDirectoryPath=path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'../templates/views')
+const partialsPath=path.join(__dirname,'../templates/partials')
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//Setup handlebars engine and views direction
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name:'Hareesh'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About Page',
+        name:'Hareesh'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help Page',
+        name:'Hareesh',
+        helpfulText:'This is some helpful text'
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address)
+    {
+        return res.send({
+            error:'You must provide an address!'
+        })
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location} = { })=>{
+        if(error){
+            return res.send({ error })
+        }
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({ error })
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products',(req,res)=>{
+    res.send({
+        products:[]
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Hareesh',
+        errorMessage:'Help article not found'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:'Hareesh',
+        errorMessage:'Page Not Found'
+    })
+})
+
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
+})
